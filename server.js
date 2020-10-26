e4a694cfb43819402ab838a07e4c2f6052741e47
fcoouto <fcoouto@users.noreply.github.com>
2020-10-26 19:35:21 +0100

    [BUG] Cached data on uploading new version

--- server.js
+++ server.js
@@ -4,11 +4,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/build/index.html');
+app.get('/*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'build/index.html'));
 });
 
 // Start the server
